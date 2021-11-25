6670538b60b5e08b4eb6f7117ba45e0eca7096ec
Peter Parente <parente@gmail.com>
2021-11-24 23:36:29 -0500

    First implementation

--- README.md
+++ README.md
@@ -1,68 +1,51 @@
-## Obsidian Sample Plugin
+# Obsidian Overdue Plugin
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+This plugin marks checkbox items as `[[Overdue]]` if you do not check them off by their due date.
+For instance, say you have items like the following:
 
-This project uses Typescript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in Typescript Definition format, which contains TSDoc comments describing what it does.
+```markdown
+# In file 2020-01-01.md
 
-**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
+- [x] Something I completed
+- [ ] Something I didn't
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Changes the default font color to red using `styles.css`.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+# In another-file.md
 
-### First time developing plugins?
+- [x] Something else I completed [[2020-01-02]]
+- [ ] Something else I forgot about [[2020-02-15]]
+```
 
-Quick starting guide for new plugin devs:
+When the plugin runs, it will make the following changes:
 
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
+```markdown
+# In file 2020-01-01.md
 
-### Releasing new releases
+- [x] Something I completed
+- [ ] Something I didn't [[Overdue]]
 
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
+# In another-file.md
 
-### Adding your plugin to the community plugin list
+- [x] Something else I completed [[2020-01-02]]
+- [ ] Something else I forgot about [[2020-02-15]] [[Overdue]]
+```
 
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
+The plugin runs automatically just after midnight to update your pages. You can also trigger the
+plugin using `Overdue: Mark overdue items` in the Obsidian command palette.
 
-### How to use
+Opening a backlinks panel for the `Overdue` page shows all of your overdue items in one location.
 
-- Clone this repo.
-- `npm i` or `yarn` to install dependencies
-- `npm run dev` to start compilation in watch mode.
+The plugin only adds `[[Overdue]]` references, never removes them. When you complete an overdue
+item, check it off and delete the `[[Overdue]]` text to stop it from appearing in the overdue
+backlinks.
 
-### Manually installing the plugin
+## Development
 
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
+```bash
+cd /path/to/vault/.obsidian/plugins
+git clone git@github.com:parente/overdue.git
+npm i
+npm run dev
+eslint main.ts
+```
 
-### Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
-
-
-### API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+See https://github.com/obsidianmd/obsidian-sample-plugin for additional details.
--- main.ts
+++ main.ts
@@ -1,137 +1,83 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+const BRACKETS_REGEX = /\[+|\]+/g;
+const DATE_REGEX = /\[\[\d{4}-\d{2}-\d{2}\]\]/g;
+const OVERDUE_REGEX = /\[\[Overdue\]\]/i;
+const TODO_REGEX = /- \[ \]/;
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+export default class Overdue extends Plugin {
+	interval: number; 
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	async markOverdue() {
+		console.log('Searching for overdue items');
+		// Compare against the start of the day in the local timezone
+		const now = new Date();
+		now.setHours(0,0,0,0);
+		for (const file of this.app.vault.getMarkdownFiles()) {
+			// Check if the note itself is dated
+			const fileDate = new Date(`${file.basename}T00:00`);
+			// Invalid dates always evaluate to false in comparisons so there's no need to check
+			// validity here.
+			const isPastDailyNote = (fileDate < now);
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+			let hasChange = false;
+			const newLines = [];
+			const content = await this.app.vault.cachedRead(file);
+			// Check all lines in the note
+			for(let line of content.split('\n')) {
+				// Only consider lines that are todo-like and not already marked overdue
+				if(line.match(TODO_REGEX) && !line.match(OVERDUE_REGEX)) {
+					if(isPastDailyNote) {
+						// Mark all todo-like lines as overdue in a note that is overdue
+						line = `${line} [[Overdue]]`;
+						hasChange = true;
+					} else {
+						// Check all dates in a line
+						for(const dateLink of line.match(DATE_REGEX) || []) {
+							const dateString = dateLink.replace(BRACKETS_REGEX, '');
+							const lineDate = new Date(`${dateString}T00:00`);
+							if(lineDate < now) {
+								// Mark the todo-like line as overdue if it has a date in the past
+								line = `${line} [[Overdue]]`;
+								hasChange = true;
+								break;
+							}
+						}
+					}
+				}
+				newLines.push(line);
+			}
+			if(hasChange) {
+				console.log(`Marking overdue item(s) in ${file.path}`)
+				await this.app.vault.modify(file, newLines.join('\n'));
+			}
+		}
+		console.log('Finished marking overdue items');
+	}
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// Add a command to mark overdue items
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'overdue-mark',
+			name: 'Mark overdue items',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				this.markOverdue();
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// Add an interval that marks overdue items during the local midnight hour.
+		// The function is idempotent so it can run safely multiple times in the hour.
+		this.interval = window.setInterval(() => {
+			const now = new Date();
+			// Run right after midnight. 
+			if(now.getHours() == 0) {
+				this.markOverdue();	
+			}
+		}, 20 * 60 * 1000);
+		this.registerInterval(this.interval);
 	}
 
 	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		window.clearInterval(this.interval);
 	}
 }
